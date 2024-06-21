a703ac6b053c353585b29e3c5b0fa857ba3490a9
gharri-wat <Gharri.Wat_Course@pm.me>
2024-06-21 14:04:56 -0600

    exercise 2.2

--- sample.c
+++ sample.c
@@ -1,7 +1,26 @@
 #include <stdio.h>
-int main() {
-	char last_name[20];
-	printf ("Enter your last name: ");
-	scanf ("%s", last_name);
-	printf("Entered last name is %s\n", last_name);
-	}
+#include <stdlib.h>
+#include <sttring.h>
+
+#define BUFSIZE 256
+
+int read_file(const char *filename){
+	
+FILE* ptr =fopen (filename,"r");
+
+if(ptr=NULL){
+printf("File cannot be opened.\n);
+	return(-1);
+}
+char buff[BUFSIZE];
+
+while (fgets(buff,BUFFSIZE,ptr) !=NULL{
+	printf("%s",buff);
+}
+return 0;
+}
+
+
+	
+
+
